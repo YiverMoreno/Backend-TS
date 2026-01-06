088b7ebc4ed9d7043785c19909b47d190af2c98f
Stiven <yiverstiven@gmail.com>
2026-01-06 17:42:20 -0500

    customer entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientModule } from './client/client.module';
 
 
 @Module({
@@ -17,6 +18,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+
+    ClientModule,
     
   ],
 })
